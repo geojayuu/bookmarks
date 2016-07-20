d00857cb500b7e016cf3135241f878b8819fb425
jayuu <jayuu.arch@gmail.com>
2016-07-20 21:27:13 +0100

    Generic read of Firefox profile to find places.sqlite. Linux only.

--- script/read_places.py
+++ script/read_places.py
@@ -7,15 +7,33 @@
 
 import os
 import sqlite3
+import configparser
 from xml.sax.saxutils import escape
 import pdb
 
-db = r'/home/{0}/.mozilla/firefox/wpatskez.default/places.sqlite'.format(
-	os.getenv('USER'))
+
+##
+# http://kb.mozillazine.org/Profile_folder_-_Firefox
+def find_mozilla_places_db():
+    # Linux only
+    # All bets are off if the profile folder is changed.
+    profileFile = r'/home/{0}/.mozilla/firefox/profiles.ini'.format(
+        os.getenv('USER'))
+    if not os.path.exists(profileFile):
+        print("Mozilla Firefox profile not found. Exiting.")
+        sys.exit(0)
+    mozProfile = configparser.ConfigParser()
+    mozProfile._interpolation = configparser.ExtendedInterpolation()
+    mozProfile.read(profileFile)
+    mozProfile.sections()
+    mozProfilePath = mozProfile.get('Profile0', 'Path')
+    profileFolder = r'/home/{0}/.mozilla/firefox'.format(os.getenv('USER'))
+    return os.path.join(profileFolder, mozProfilePath, r'places.sqlite')
+
 
 ##
 # Found a bookmark which needs processing
-def process_bookmark(foreign_key):
+def process_bookmark(foreign_key, db):
     conn=sqlite3.connect(db)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
@@ -25,9 +43,11 @@ def process_bookmark(foreign_key):
         rs[r'href'] = escape(row['url'])
     return rs
 
+
 ##
 # Found a folder which needs processing
-def process_folder(an_id):
+def process_folder(an_id, db):
+    #pdb.set_trace()
     conn=sqlite3.connect(db)
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
@@ -41,33 +61,38 @@ def process_folder(an_id):
     # todo FOLDER info. should be set here.
     for row in c.execute(sql):
         if row['fk'] is None:
-            # Call self.
-            process_folder(row['id'])
+            # Call self
+            process_folder(row['id'], db)
         else:
             # Process the bookmark
             print("<bookmark href=\"{0}\" bmid=\"{1}\">".format(
-                process_bookmark(row['fk'])['href'], row['guid']))
+                process_bookmark(row['fk'], db)['href'], row['guid']))
             print("<title>{0}</title>".format(escape(row['title'])))
             print("</bookmark>")
     conn.close()
     print("</folder>")
 
+
 ##
 #
 def main(argv=None):
+    # Find Mozilla profile dir
+    db = find_mozilla_places_db() 
     # Set up file
-	print("""<?xml version="1.0"?>
-	<!DOCTYPE xbel 
-	PUBLIC "+//IDN python.org//DTD XML Bookmark Exchange Language 1.0//EN//XML" 
-	"http://www.python.org/topics/xml/dtds/xbel-1.0.dtd">
-<xbel version="1.0">""")
+    print("""
+    <?xml version="1.0"?>
+    <!DOCTYPE xbel 
+    PUBLIC "+//IDN python.org//DTD XML Bookmark Exchange Language 1.0//EN//XML" 
+    "http://www.python.org/topics/xml/dtds/xbel-1.0.dtd">
+    <xbel version="1.0">""")
     # We want to process all folders in 'All Bookmarks'
     # - Bookmarks Menu/Mozilla Firefox      (id 27)
     # - Unsorted Bookmarks                  (id 5)
-	folder_ids = ['27', '5']
-	for folder in folder_ids:
-		process_folder(folder)
-	print(r'</xbel>')
+    folder_ids = ['27', '5']
+    for folder in folder_ids:
+        process_folder(folder, db)
+    print(r'</xbel>')
+
 
 if __name__ == '__main__':
     main()
